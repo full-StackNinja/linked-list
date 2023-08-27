93d3b0e0a56e332008d9b4cba76caafbff504d0e
Imran Hussain <imranhussain399@gmail.com>
2023-08-27 21:22:25 +0500

    Define Node and linked List classes with append and prepend methods

---
+++ linkedList.js
@@ -0,0 +1,53 @@
+// Define Node
+class Node {
+  constructor(node) {
+    this.val = node;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Add element to the end
+  append(element) {
+    const newNode = new Node(element);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+    this.size++;
+  }
+
+  // Add element to the start of the list
+  prepend(element) {
+    const newNode = new Node(element);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let next = this.head;
+      // let next = current.next;
+      this.head = newNode;
+      let current = this.head;
+      current.next = next;
+    }
+    this.size++;
+  }
+  
+}
+const elements = [3, 2, 4, 5, 6, 8, 1, 0];
+const newList = new LinkedList();
+for (element of elements) {
+  newList.append(element);
+}
+
+newList.prepend(11);
+console.log(newList);
